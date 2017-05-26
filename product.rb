507d9ae47c6c60b8279516188d3f7b88767bb420
Ronen Annason <ronenannason83@gmail.com>
2017-05-26 14:26:20 -0400

    created class product and shopping cart - set up initialize method for class product, set up empty array for class shopping cart

---
+++ product.rb
@@ -0,0 +1,34 @@
+#Shopping Cart and Product Project
+
+##########REQUIRE?##################
+
+
+
+
+
+
+################Class##############################
+class Product
+end
+
+
+
+################Initialize_Method######################
+def initialize(name, base_price, tax_rate)
+  @name = name
+  @base_price = base_price
+  @tax_rate = tax_rate
+
+end
+
+
+
+
+
+##############Class Methods###################
+
+
+
+
+
+##############Instance Methods################
---
+++ shopping_cart.rb
@@ -0,0 +1,32 @@
+#Shopping Cart and Product Project
+
+
+################Class##############################
+class Shopping_cart
+
+
+end
+
+def self.create
+end
+
+
+
+################Initialize_Method######################
+def Initialize
+
+
+  @my_shopping_cart =[]
+
+
+
+
+
+
+##############Class Methods###################
+
+
+
+
+
+##############Instance Methods################
