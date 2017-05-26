34b087a1051e1daee833ba2ede0cf675c602e7b4
Ronen Annason <ronenannason83@gmail.com>
2017-05-26 15:26:11 -0400

    added remove method to shopping cart class

--- shopping_cart.rb
+++ shopping_cart.rb
@@ -35,7 +35,9 @@ class ShoppingCart
     @products << product
   end
 
-
+  def remove(product)
+    @products.delete(product)
+  end
 
 
 
@@ -49,9 +51,12 @@ end
 
 ##############Testing Output#########################3
 soap = Product.new("soap", 2.00)
+rope = Product.new("rope", 3.00)
 my_cart = ShoppingCart.new
 puts soap.total_price
 
 my_cart.add(soap)
+my_cart.remove(rope)
+my_cart.add(rope)
 
 puts my_cart.inspect
